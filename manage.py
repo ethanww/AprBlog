4386e38a34bf181fabfc9325ab1257e5e256d255
ethanw <ethan_w@aliyun.com>
2017-04-24 22:19:23 +0800

    add manage.py

--- manage.py
+++ manage.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
-import os
-from app import create_app,db
-from app.models import Post
-from flask_script import Manager,Shell,Server
 
-app=create_app(os.getenv('FLASK_CONFIG') or 'default')
-# app=create_app()
-manager=Manager(app)
+from app import app,db,Post
+from flask_script import Manager,Server,Shell
 
+manage=Manager(app)
 def make_shell_context():
-    return dict(app=app,db=db,Post=Post)
-manager.add_command('shell',Shell(make_context=make_shell_context))
-manager.add_command('run',Server(use_debugger=True,host='127.0.0.1',port=5007))
+	return dict(app=app,db=db,Post=Post)
 
-if __name__ == '__main__':
-    manager.run()
+manage.add_command('run',Server(use_debugger=True,host='127.0.0.1',port=5000))
+manage.add_command('shell',Shell(make_context=make_shell_context))
+
+if __name__=='__main__':
+    manage.run()
